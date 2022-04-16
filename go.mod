9edbb018ac6b40d5e8918acd25252b98b9d019f7
kaiya <x.k@msn.com>
2022-04-15 23:49:16 -0700

    update

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/kaiya/go-dev
+
+go 1.18
--- new_print.go
+++ new_print.go
@@ -1,13 +1,24 @@
 package main
 
 import (
+	"encoding/csv"
 	"fmt"
+	"os"
 	"time"
 )
 
 func main() {
 	fmt.Println("new fmt.Println, should have message above this line")
 	go testNewProc()
+	file, err := os.Create("newfile.csv")
+	if err != nil {
+		fmt.Println("create file error: ", err.Error())
+	}
+	defer file.Close()
+	writer := csv.NewWriter(file)
+	writer.Write([]string{"hello", "world"})
+	writer.Flush()
+
 }
 
 func testNewProc() {
